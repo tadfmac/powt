ee90474d4cc9b1eb98d753358649d7d2841b6a13
tadfmac <tadfmac@gmail.com>
2024-05-20 14:45:06 +0900

    20240520-2

--- example/server/app.mjs
+++ example/server/app.mjs
@@ -6,9 +6,9 @@ import https from "https";
 import express from 'express';
 import cors from 'cors';
 import config from "./config.mjs";
-import powtServer from "./powtserver.mjs";
-import protocol from "./www/protocol.mjs";
-import LOG from "./www/log.mjs";
+import powtServer from "../../lib/powtserver.mjs";
+import protocol from "../common/protocol.mjs";
+import LOG from "../../lib/log.mjs";
 
 const Log = new LOG(null);
 
@@ -17,7 +17,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-app.use(config.path,express.static("./www"));
+app.use(config.path,express.static("./example/client"));
+app.use("/common",express.static("./example/common"));
+app.use("/lib",express.static("./lib"));
 
 const webServer = https.createServer({cert: config.cert, key: config.key},app);
 webServer.listen(config.PORT);
